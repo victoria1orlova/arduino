4c642afb119af03315bae10854cab230690b6032
VikaPaw0990 <victoria1orlova@gmail.com>
2020-09-07 06:24:46 -0700

    Arduino with COM

--- FirstProject/FirstProject.ino
+++ FirstProject/FirstProject.ino
@@ -1,10 +1,18 @@
+byte lol;
 void setup() {
+  Serial.begin(9600);
   pinMode(13,1);
 }
 
 void loop() {
-  digitalWrite(13,1);
-  delay(1500);
-  digitalWrite(13,0);
-  delay(500);
+  if (Serial.available() > 0){
+    lol = Serial.read();
+    if(lol == '1'){
+      digitalWrite(13,1);
+    }
+    if(lol == '0'){
+      digitalWrite(13,0);
+    }
+  }
+  delay(3);
 }
